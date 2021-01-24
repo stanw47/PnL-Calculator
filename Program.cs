45debd0b44ebc100aac5ce4b601f949120ffadad
Stanley Williamson <stanleywilliamson47@gmail.com>
2021-01-24 16:24:13 -0500

    working calculator

--- Program.cs
+++ Program.cs
@@ -6,13 +6,23 @@ namespace Profit_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the P&L Calculator! To get started, type in the purchase/entry price of the asset or item");
+            Console.WriteLine("Welcome to the P&L Calculator! To get started, type in the purchase/entry/average price of the asset or item.");
             double price1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("What did you sell it for?");
+            Console.WriteLine("What did you sell it for? Type in selling/exit/average price.");
             double sale1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("How many units did you buy? If you took a short on a stock, type in a negative number.");
+            Console.WriteLine("How many units? If you took a short position, enter a negative number.");
             double units1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine();
+            Console.WriteLine("What were your total expenses related to the transaction");
+            double exp1 = Convert.ToDouble(Console.ReadLine());
+            double pnl1 = ((units1 * (sale1 - price1)) - exp1);
+            if (pnl1 > 0)
+            {
+                Console.WriteLine("Your profit is " + pnl1);
+            }
+            else if (pnl1 < 0)
+            {
+                Console.WriteLine("Your Loss is " + pnl1);
+            }
         }
     }
 }
