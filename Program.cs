b23b04b3d50ff603d559579500065d4324f157d8
Stanley Williamson <stanleywilliamson47@gmail.com>
2021-01-24 15:53:17 -0500

    first

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Profit_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the P&L Calculator! To get started, type in the purchase/entry price of the asset or item");
+            double price1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What did you sell it for?");
+            double sale1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("How many units did you buy? If you took a short on a stock, type in a negative number.");
+            double units1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
         }
     }
 }
